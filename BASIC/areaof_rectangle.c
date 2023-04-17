be4662a2ebf5566112d615cc6eb80ea5bea47a80
SiddharthBahuguna <112819453+SiddharthBahuguna@users.noreply.github.com>
2023-04-17 21:46:37 +0530

    Add files via upload

---
+++ BASIC/areaof_rectangle.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+	float l,b,area;
+	printf("Enter the value of length & breadth of a rectangle:");
+	scanf("%f %f",&l,&b);
+	area=l*b;
+	printf("The area of rectangle is:%f",area);
+	getch();
+	
+}
