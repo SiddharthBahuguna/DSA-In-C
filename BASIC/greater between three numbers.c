a4f3d9f68b141c19b62b3be0de07eddad84ada03
SiddharthBahuguna <112819453+SiddharthBahuguna@users.noreply.github.com>
2023-04-24 21:19:07 +0530

    Add files via upload

---
+++ BASIC/greater between three numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("Enter three numbers:");
+	scanf("%d %d %d",&a,&b,&c);
+	
+	if(a>b)
+	if(a>c)
+	printf("%d",a);
+	
+	else
+	printf("%d",c);
+	
+	else if (b>c)
+	printf("%d",b);
+	
+	else
+	printf("%d",c);
+	getch();
+}
