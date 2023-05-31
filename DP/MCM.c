e25dca31b5f17458dfaaecbdc583ad96902381a1
SiddharthBahuguna <112819453+SiddharthBahuguna@users.noreply.github.com>
2023-06-01 00:28:58 +0530

    Add files via upload

---
+++ DP/MCM.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<limits.h>
+
+int MCM(int p[],int i,int j){
+	if(i==j) return 0;
+	int k;
+	int min=INT_MAX;
+	int count;
+	
+	for(k=i;k<j;k++)
+	{
+		count=MCM(p,i,k)+MCM(p,k+1,j)+p[i-1]*p[k]*p[j];
+		if(count < min) min=count;
+	}
+	return min;
+}
+int main(){
+	int arr[]={1,2,3,4,3};
+	int N=sizeof(arr)/sizeof(arr[0]);
+	printf("Min no. of multiplication %d",MCM(arr,1,N-1));
+}
